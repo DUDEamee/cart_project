e8c5cd6435b36f712c4eac2645e278aec33422e4
DUDEamee <ameenameen8701@gmail.com>
2024-05-04 20:41:58 +0530

    add: secret key to env

--- main/settings.py
+++ main/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)uc#8y5s0t*a#s)_0b98p5a3uju*dp#1)5-)-k7$#t!g3d)2n'
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
