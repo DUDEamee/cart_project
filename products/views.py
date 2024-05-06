85229ba0b08aa32dde001391cc72f04a741efe61
DUDEamee <ameenameen8701@gmail.com>
2024-05-06 12:26:35 +0530

    added:Products paginator

--- products/views.py
+++ products/views.py
@@ -15,7 +15,7 @@ def list_products(request):
     if request.GET:
         page = request.GET.get("page", 1)
     product_list = product.objects.all()
-    product_paginator = Paginator(product_list, 2)
+    product_paginator = Paginator(product_list, 10)
     product_list = product_paginator.get_page(page)
     context = {"products": product_list}
 
